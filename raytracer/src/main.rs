47cb361a5a17e081abb583ab5315e8ec47c0bece
Xinwei Qiang <qiangxinwei@sjtu.edu.cn>
2023-06-23 19:13:46 +0800

    fuzzed metal

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -86,8 +86,8 @@ fn main() {
 
     let material_ground = Arc::new(Lambertian::new(Color3::new(0.8, 0.8, 0.0)));
     let material_center = Arc::new(Lambertian::new(Color3::new(0.7, 0.3, 0.3)));
-    let material_left = Arc::new(Metal::new(Color3::new(0.8, 0.8, 0.8)));
-    let material_right = Arc::new(Metal::new(Color3::new(0.8, 0.6, 0.2)));
+    let material_left = Arc::new(Metal::new(Color3::new(0.8, 0.8, 0.8), 0.3));
+    let material_right = Arc::new(Metal::new(Color3::new(0.8, 0.6, 0.2), 1.0));
 
     world.add(Box::new(Sphere::new(
         Point3::new(0., -100.5, -1.),
--- raytracer/src/material.rs
+++ raytracer/src/material.rs
@@ -43,11 +43,15 @@ impl Material for Lambertian {
 
 pub struct Metal {
     albedo: Color3,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color3) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color3, f: f64) -> Self {
+        Self {
+            albedo,
+            fuzz: if f < 1. { f } else { 1. },
+        }
     }
 }
 
@@ -60,7 +64,7 @@ impl Material for Metal {
         scattered: &mut Ray,
     ) -> bool {
         let reflected = Vec3::reflect(_r_in.direction().unit(), rec.normal);
-        *scattered = Ray::new(rec.p, reflected);
+        *scattered = Ray::new(rec.p, reflected + self.fuzz * Vec3::random_in_unit_sphere());
         *attenuation = self.albedo;
         scattered.direction() * rec.normal > 0.
     }

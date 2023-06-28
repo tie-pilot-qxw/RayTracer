68fd65224d2f8b97d0c063fb0611dcb4da4aff62
Xinwei Qiang <qiangxinwei@sjtu.edu.cn>
2023-06-28 10:32:22 +0800

    test-5

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -463,7 +463,7 @@ fn final_scene() -> HittableList {
 }
 
 fn main() {
-    const THREAD_NUM: usize = 4;
+    const THREAD_NUM: usize = 5;
 
     // get environment variable CI, which is true for GitHub Actions
     let is_ci = is_ci();
